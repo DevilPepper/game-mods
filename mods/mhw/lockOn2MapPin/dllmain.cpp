a4800405b8bbcca8ae5b96177d9ff8f9089bb52d
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-06 03:39:18 -0500

    refactor(LockOn2MapPin): access memory directly

--- mods/mhw/lockOn2MapPin/dllmain.cpp
+++ mods/mhw/lockOn2MapPin/dllmain.cpp
@@ -15,11 +15,11 @@
 
 using loader::LOG;
 using loader::DEBUG;
-using loader::WARN;
 using std::vector;
-using std::optional;
 using std::string;
 using MHW::Targets::pinMap;
+
+using namespace stuff::memory;
 
 vector<intptr_t> lockOnOffsets{ 0x05073EC0, 0x50, 0x80, 0x80, -0x7C };
 vector<intptr_t> unknown{ 0x5224B80, 0x13FA0 };
@@ -28,15 +28,14 @@ vector<intptr_t> numMonsters{ 0x05073EC0, 0x50, 0x80, 0x80, 0x120, 0x958 };
 vector<intptr_t> nonZero{ 0x5073DA0, 0x128 };
 vector<intptr_t> pinnedMonsterPtr{ 0x5073DA0, 0x148 };
 
-optional<stuff::memory::Helper> mhw;
+intptr_t mhw = 0x140000000;
 
 bool isOnTheLoose(intptr_t monsterAddr) {
     // Shameless copy and paste from Hunter Pie    
     monsterAddr += 0x61C8;
-    int actionId = -1;
+    int actionId = *(int*)(monsterAddr + 0xB0);
 
-    mhw->readMem(monsterAddr + 0xB0, &actionId, sizeof(int));
-    LOG(WARN) << std::hex << "actionId @ 0x" << (monsterAddr + 0xB0) << ": " << actionId;
+    LOG(DEBUG) << std::hex << "actionId @ 0x" << (monsterAddr + 0xB0) << ": " << actionId;
 
     vector<intptr_t> monsterAction{
         monsterAddr + (2*8) + 0x68,
@@ -46,20 +45,18 @@ bool isOnTheLoose(intptr_t monsterAddr) {
     };
 
     intptr_t actionPtr = NULL;
-    unsigned int offset = 0;
-
-    auto actionAddr = mhw->readMemNoBase(monsterAction, &actionPtr, sizeof(intptr_t));
-    LOG(WARN) << std::hex << "monster action @ 0x" << actionAddr << ": " << actionPtr;
-    mhw->readMem(actionPtr + 3, &offset, sizeof(unsigned int));
-    LOG(WARN) << std::hex << "action offset @ 0x" << (actionPtr + 3) << ": " << offset;
+    auto actionAddr = readMem(0, monsterAction, actionPtr);
+    LOG(DEBUG) << std::hex << "monster action @ 0x" << actionAddr << ": " << actionPtr;
+    
+    auto offset = *(unsigned int*)(actionPtr + 3);
+    LOG(DEBUG) << std::hex << "action offset @ 0x" << (actionPtr + 3) << ": " << offset;
 
     vector<intptr_t> actionRef{ actionPtr + offset + 7 + 8, 0 };
 
-    char actionString[65];
-    actionString[64] = 0;
-    auto strAddr = mhw->readMemNoBase(actionRef, actionString, 64);
-    string action(actionString);
-    LOG(WARN) << std::hex << "action string @ 0x" << strAddr << ": " << actionString << "...toString(): " << action;
+    auto strAddr = (char*)followPointers(0, actionRef);
+    string action(strAddr);
+
+    LOG(DEBUG) << std::hex << "action string @ 0x" << strAddr << ": " << action;
 
     return  action.find("Capture") == string::npos
         && (action.find("Die") == string::npos || action.find("DieSleep") != string::npos)
@@ -68,24 +65,23 @@ bool isOnTheLoose(intptr_t monsterAddr) {
 
 int getRealIndex(intptr_t address, int idx) {
     address -= 0x19F8;
-    mhw->readMem(address + ((intptr_t)idx*8), &idx, sizeof(int));
-    return idx;
+    return *(int*)(address + ((intptr_t)idx*8));
 }
 
 void lockOn2MapPin() {
     int idx = -1;
-    auto lockOnAddr = mhw->readMem(lockOnOffsets, &idx, sizeof(int));
-    LOG(WARN) << std::hex << "lockOn @ 0x" << lockOnAddr << ": " << idx;
+    auto lockOnAddr = readMem(mhw, lockOnOffsets, idx);
+    LOG(DEBUG) << std::hex << "lockOn @ 0x" << lockOnAddr << ": " << idx;
 
     uintptr_t p0 = 0;
-    auto unknownAddr = mhw->readMem(unknown, &p0, sizeof(uintptr_t));
-    LOG(WARN) << std::hex << "p0 @ 0x" << unknownAddr << ": " << p0;
+    auto unknownAddr = readMem(mhw, unknown, p0);
+    LOG(DEBUG) << std::hex << "p0 @ 0x" << unknownAddr << ": " << p0;
 
     uintptr_t monster = 0;
     if (idx != -1) {
         int nMonsters = -1;
-        auto numMonstersAddr = mhw->readMem(numMonsters, &nMonsters, sizeof(int));
-        LOG(WARN) << std::hex << "numMonsters @ 0x" << numMonstersAddr << ": " << nMonsters;
+        auto numMonstersAddr = readMem(mhw, numMonsters, nMonsters);
+        LOG(DEBUG) << std::hex << "numMonsters @ 0x" << numMonstersAddr << ": " << nMonsters;
 
         if (nMonsters > 0) {
             vector<uintptr_t> monsters;
@@ -94,8 +90,8 @@ void lockOn2MapPin() {
                 auto offset = (nMonsters - i) * 8;
                 auto monster_i = lastMonster;
                 monster_i[2] = offset;
-                auto monsterAddr = mhw->readMem(monster_i, &monster, sizeof(uintptr_t));
-                LOG(WARN) << std::hex << "monster found @ 0x" << monsterAddr << ": " << monster;
+                auto monsterAddr = readMem(mhw, monster_i, monster);
+                LOG(DEBUG) << std::hex << "monster found @ 0x" << monsterAddr << ": " << monster;
                 monsters.push_back(monster);
             }
 
@@ -109,12 +105,12 @@ void lockOn2MapPin() {
         }
     }
     else {
-        auto nonZeroAddr = mhw->readMem(nonZero, &monster, sizeof(uintptr_t));
-        LOG(WARN) << std::hex << "non-zero @ 0x" << nonZeroAddr << ": " << monster;
+        auto nonZeroAddr = readMem(mhw, nonZero, monster);
+        LOG(DEBUG) << std::hex << "non-zero @ 0x" << nonZeroAddr << ": " << monster;
 
         if (monster != 0) {
-            auto pinnedMonsterAddr = mhw->readMem(pinnedMonsterPtr, &monster, sizeof(uintptr_t));
-            LOG(WARN) << std::hex << "pinned monster @ 0x" << pinnedMonsterAddr << ": " << monster;
+            auto pinnedMonsterAddr = readMem(mhw, pinnedMonsterPtr, monster);
+            LOG(DEBUG) << std::hex << "pinned monster @ 0x" << pinnedMonsterAddr << ": " << monster;
             pinMap(p0, monster, 1);
         }
     }
@@ -122,9 +118,6 @@ void lockOn2MapPin() {
 
 CreateHook(MHW::Targets::lockOnInc, LockOnIncHook, void, uintptr_t ptr) {
     original(ptr);
-    if (!mhw) {
-        mhw = stuff::memory::Helper("MONSTER HUNTER: WORLD(421409)", 0x140000000);
-    }
     lockOn2MapPin();
 }
 
