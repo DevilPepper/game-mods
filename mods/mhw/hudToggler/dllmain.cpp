806542244839cfb6c6ea2e210a0a5176f0beb214
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-06 03:20:38 -0500

    fix(HUDToggler): update offsets for 15.11.00
    also access memory directly instead of using Read/WriteProcessMemory()

--- mods/mhw/hudToggler/dllmain.cpp
+++ mods/mhw/hudToggler/dllmain.cpp
@@ -7,33 +7,25 @@
 
 #include "loader.h"
 #include "util.h"
-#include "ghidra_export.h"
 #pragma comment (lib, "loader.lib")
 
 #include "stuff.h"
 #include "Gamepad.h"
 #pragma comment (lib, "stuff.lib")
 
-#define DIRECTINPUT_VERSION 0x0800
-#include <dinput.h>
-#pragma comment (lib, "dinput8.lib")
-#pragma comment (lib, "dxguid.lib")
-
 using loader::LOG;
 using loader::DEBUG;
-using loader::ERR;
 using std::string;
 using std::vector;
 using stuff::memory::readMem;
 using stuff::memory::writeMem;
 
-typedef void(__fastcall* PointerBiConsumer)(longlong, longlong);
+typedef void(__fastcall* PointerBiConsumer)(long long, long long);
 
-HANDLE procHandle = NULL;
 intptr_t mhw = 0x140000000;
 
 vector<intptr_t> subtitle_setting{ 0x5073E80, 0x10A };
-vector<intptr_t> subtitle_show{ 0x5224B90, 0x13e20, 0x332c };
+vector<intptr_t> subtitle_show{ 0x5224B80, 0x13e20, 0x332c };
 vector<intptr_t> hud_settings{ 0x5073E80, 0xa8, 0x151F8C };
 
 long long xor_settings = 0x0001000001000101;
@@ -41,33 +33,26 @@ long long xor_settings = 0x0001000001000101;
 bool SelectPressed = false;
 bool L1Pressed = false;
 
-void setHandle() {
-    auto hGameWindow = FindWindow(NULL, "MONSTER HUNTER: WORLD(421409)");
-    DWORD pid = NULL;
-    GetWindowThreadProcessId(hGameWindow, &pid);
-
-    procHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
-}
-
 void toggleSubtitles() {
 
     bool subs_setting;
-    readMem(procHandle, mhw, subtitle_setting, &subs_setting, sizeof(bool));
+    readMem(mhw, subtitle_setting, subs_setting);
     subs_setting ^= true;
-    writeMem(procHandle, mhw, subtitle_setting, &subs_setting, sizeof(bool));
+    writeMem(mhw, subtitle_setting, subs_setting);
 
     bool subs_show;
-    readMem(procHandle, mhw, subtitle_show, &subs_show, sizeof(bool));
+    readMem(mhw, subtitle_show, subs_show);
     subs_show ^= true;
-    writeMem(procHandle, mhw, subtitle_show, &subs_show, sizeof(bool));
+    writeMem(mhw, subtitle_show, subs_show);
 }
 
 void toggleHUD() {
     long long hud;
-    readMem(procHandle, mhw, hud_settings, &hud, sizeof(long long));
+    auto hudAddr = readMem(mhw, hud_settings, hud);
+    LOG(DEBUG) << std::hex << "Before @ 0x" << hudAddr << ": " << hud;
     hud ^= xor_settings;
-    writeMem(procHandle, mhw, hud_settings, &hud, sizeof(long long));
-
+    writeMem(mhw, hud_settings, hud);
+    LOG(DEBUG) << std::hex << "After @ 0x" << hudAddr << ": " << hud;
 }
 
 enum BTN_ACTION { PRESSED, RELEASED, LONG_PRESS, LONG_RELEASE };
@@ -94,25 +79,20 @@ BTN_ACTION btnPressed(Button btn, bool& pressed, uint32_t buttons) {
     }
 }
 
-CreateHook((PointerBiConsumer)0x1418e8ea0, PollCtrlHook, void, longlong p1, longlong p2) {
-    Gamepad* gamepad = (Gamepad*)p1;
-
+CreateHook((PointerBiConsumer)0x1418e8ea0, PollCtrlHook, void, long long p1, long long p2) {
     original(p1, p2);
 
-    if(procHandle == NULL) {
-        setHandle();
-    }
-
+    Gamepad* gamepad = (Gamepad*)p1;
     if (btnPressed(Button::Select, SelectPressed, gamepad->buttons) == PRESSED) {
-        LOG(ERR) << std::hex << "Toggle Subtitles: " << Buttons[Button::Select];
+        LOG(DEBUG) << std::hex << "Toggle Subtitles: " << Buttons[Button::Select];
         toggleSubtitles();
     }
     auto l1 = btnPressed(Button::L1, L1Pressed, gamepad->buttons);
     if (l1 == PRESSED) {
-        LOG(ERR) << std::hex << "HUD On: " << Buttons[Button::L1];
+        LOG(DEBUG) << std::hex << "HUD On: " << Buttons[Button::L1];
         toggleHUD();
     } else if(l1 == RELEASED) {
-        LOG(ERR) << "HUD Off";
+        LOG(DEBUG) << "HUD Off";
         toggleHUD();
     }
    return;
