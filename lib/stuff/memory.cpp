6b80d2a3ca9785c8921d592d571e48d7728690be
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-06 11:46:31 -0500

    refactor(memory): delete unused helper class

--- lib/stuff/memory.cpp
+++ lib/stuff/memory.cpp
@@ -20,57 +20,5 @@ namespace stuff {
             addy += offsets[i];
             return addy;
         }
-
-        void Helper::setProc(string exe) {
-            auto hGameWindow = FindWindow(NULL, exe.c_str());
-            DWORD pid = NULL;
-            GetWindowThreadProcessId(hGameWindow, &pid);
-
-            proc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
-        }
-        intptr_t Helper::followPointers(intptr_t base, vector<intptr_t>& offsets) {
-            auto addy = base;
-            int i = 0;
-            for (; i < offsets.size() - 1; i++) {
-                ReadProcessMemory(proc, (LPVOID)(addy + offsets[i]), &addy, sizeof(intptr_t), NULL);
-            }
-            addy += offsets[i];
-            return addy;
-        }
-        Helper::Helper(string exe) : base(0) {
-            setProc(exe);
-        }
-        Helper::Helper(string exe, intptr_t base) : base(base) {
-            setProc(exe);
-        }
-
-        intptr_t Helper::readMem(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            auto addy = this->followPointers(base, offsets);
-            this->readMem(addy, buffer, size);
-            return addy;
-        }
-
-        intptr_t Helper::writeMem(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            auto addy = this->followPointers(base, offsets);
-            this->writeMem(addy, buffer, size);
-            return addy;
-        }
-
-        void Helper::readMem(intptr_t address, LPVOID buffer, SIZE_T size) {
-            ReadProcessMemory(proc, (LPVOID)address, buffer, size, NULL);
-        }
-        void Helper::writeMem(intptr_t address, LPVOID buffer, SIZE_T size) {
-            WriteProcessMemory(proc, (LPVOID)address, buffer, size, NULL);
-        }
-        intptr_t Helper::readMemNoBase(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            auto addy = this->followPointers(0, offsets);
-            this->readMem(addy, buffer, size);
-            return addy;
-        }
-        intptr_t Helper::writeMemNoBase(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            auto addy = this->followPointers(0, offsets);
-            this->writeMem(addy, buffer, size);
-            return addy;
-        }
     }
 }
--- lib/stuff/memory.h
+++ lib/stuff/memory.h
@@ -22,23 +22,5 @@ namespace stuff {
             *(T*)addy = buffer;
             return addy;
         }
-
-		class Helper {
-		private:
-			HANDLE proc;
-			intptr_t base;
-			void setProc(string exe);
-        	intptr_t followPointers(intptr_t base, vector<intptr_t>& offsets);
-		public:
-			Helper() = delete;
-			Helper(string exe);
-			Helper(string exe, intptr_t base);
-			intptr_t readMem(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size);
-			intptr_t writeMem(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size);
-			void readMem(intptr_t address, LPVOID buffer, SIZE_T size);
-			void writeMem(intptr_t address, LPVOID buffer, SIZE_T size);
-			intptr_t readMemNoBase(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size);
-			intptr_t writeMemNoBase(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size);
-		};
 	}
 }
