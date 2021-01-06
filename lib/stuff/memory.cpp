ea273b635b01cfebce7d4aa7d1ff9ee1def4d2cf
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-06 03:20:38 -0500

    fix(HUDToggler): update offsets for 15.11.00
    also access memory directly instead of using Read/WriteProcessMemory()

--- lib/stuff/memory.cpp
+++ lib/stuff/memory.cpp
@@ -11,25 +11,13 @@ using namespace std;
 
 namespace stuff {
     namespace memory {
-        intptr_t readMem(HANDLE proc, intptr_t base, vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            intptr_t addy = base;
+        intptr_t followPointers(intptr_t base, vector<intptr_t>& offsets) {
+            auto addy = base;
             int i = 0;
             for (; i < offsets.size() - 1; i++) {
-                ReadProcessMemory(proc, (LPVOID)(addy + offsets[i]), &addy, sizeof(intptr_t), NULL);
+                addy = *(intptr_t*)(addy + offsets[i]);
             }
             addy += offsets[i];
-            ReadProcessMemory(proc, (LPVOID)(addy), buffer, size, NULL);
-            return addy;
-        }
-
-        intptr_t writeMem(HANDLE proc, intptr_t base, vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            intptr_t addy = base;
-            int i = 0;
-            for (; i < offsets.size() - 1; i++) {
-                ReadProcessMemory(proc, (LPVOID)(addy + offsets[i]), &addy, sizeof(intptr_t), NULL);
-            }
-            addy += offsets[i];
-            WriteProcessMemory(proc, (LPVOID)(addy), buffer, size, NULL);
             return addy;
         }
 
@@ -40,7 +28,15 @@ namespace stuff {
 
             proc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
         }
-
+        intptr_t Helper::followPointers(intptr_t base, vector<intptr_t>& offsets) {
+            auto addy = base;
+            int i = 0;
+            for (; i < offsets.size() - 1; i++) {
+                ReadProcessMemory(proc, (LPVOID)(addy + offsets[i]), &addy, sizeof(intptr_t), NULL);
+            }
+            addy += offsets[i];
+            return addy;
+        }
         Helper::Helper(string exe) : base(0) {
             setProc(exe);
         }
@@ -49,11 +45,15 @@ namespace stuff {
         }
 
         intptr_t Helper::readMem(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            cout << std::hex << proc << "|" << base;
-            return stuff::memory::readMem(proc, base, offsets, buffer, size);
+            auto addy = this->followPointers(base, offsets);
+            this->readMem(addy, buffer, size);
+            return addy;
         }
+
         intptr_t Helper::writeMem(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            return stuff::memory::writeMem(proc, base, offsets, buffer, size);
+            auto addy = this->followPointers(base, offsets);
+            this->writeMem(addy, buffer, size);
+            return addy;
         }
 
         void Helper::readMem(intptr_t address, LPVOID buffer, SIZE_T size) {
@@ -63,10 +63,14 @@ namespace stuff {
             WriteProcessMemory(proc, (LPVOID)address, buffer, size, NULL);
         }
         intptr_t Helper::readMemNoBase(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            return stuff::memory::readMem(proc, 0, offsets, buffer, size);
+            auto addy = this->followPointers(0, offsets);
+            this->readMem(addy, buffer, size);
+            return addy;
         }
         intptr_t Helper::writeMemNoBase(vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size) {
-            return stuff::memory::writeMem(proc, 0, offsets, buffer, size);
+            auto addy = this->followPointers(0, offsets);
+            this->writeMem(addy, buffer, size);
+            return addy;
         }
     }
 }
--- lib/stuff/memory.h
+++ lib/stuff/memory.h
@@ -9,14 +9,26 @@ using std::string;
 
 namespace stuff {
 	namespace memory {
-		intptr_t readMem(HANDLE proc, intptr_t base, vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size);
-		intptr_t writeMem(HANDLE proc, intptr_t base, vector<intptr_t>& offsets, LPVOID buffer, SIZE_T size);
+        intptr_t followPointers(intptr_t base, vector<intptr_t>& offsets);
+        template<typename T>
+        intptr_t readMem(intptr_t base, vector<intptr_t>& offsets, T& buffer) {
+            auto addy = followPointers(base, offsets);
+            buffer = *(T*)addy;
+            return addy;
+        }
+        template<typename T>
+        intptr_t writeMem(intptr_t base, vector<intptr_t>& offsets, T& buffer) {
+            auto addy = followPointers(base, offsets);
+            *(T*)addy = buffer;
+            return addy;
+        }
 
 		class Helper {
 		private:
 			HANDLE proc;
 			intptr_t base;
 			void setProc(string exe);
+        	intptr_t followPointers(intptr_t base, vector<intptr_t>& offsets);
 		public:
 			Helper() = delete;
 			Helper(string exe);
