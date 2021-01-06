06a57fc2067b43c9218d1562fe1b437dca27af51
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-05 23:52:06 -0500

    refactor(HUDToggler): hook to controller polling loop and cast param 1 as Gamepad pointer

--- mods/mhw/hudToggler/dllmain.cpp
+++ mods/mhw/hudToggler/dllmain.cpp
@@ -11,6 +11,7 @@
 #pragma comment (lib, "loader.lib")
 
 #include "stuff.h"
+#include "Gamepad.h"
 #pragma comment (lib, "stuff.lib")
 
 #define DIRECTINPUT_VERSION 0x0800
@@ -26,27 +27,19 @@ using std::vector;
 using stuff::memory::readMem;
 using stuff::memory::writeMem;
 
-typedef HRESULT(__fastcall* CreateDevice)(REFGUID, LPDIRECTINPUTDEVICE*, LPUNKNOWN);
-CreateDevice fnCreateDevice = nullptr;
-
-IDirectInput8* device = nullptr;
-vector<LPDIRECTINPUTDEVICE8> gamepads;
-unsigned char gInput[32];
-
-LPDIRECTINPUTDEVICE8 kb = nullptr;
-LPDIRECTINPUTDEVICE8 mouse = nullptr;
-std::future<void> ctrlThread;
+typedef void(__fastcall* PointerBiConsumer)(longlong, longlong);
 
 HANDLE procHandle = NULL;
-HWND hGameWindow = NULL;
-// TODO: Doesn't work. Need to unprotect memory...
 intptr_t mhw = 0x140000000;
 
-vector<intptr_t> subtitle_setting{ 0x5073E70, 0x10A };
-vector<intptr_t> subtitle_show{ 0x5224B80, 0x13e20, 0x332c };
-vector<intptr_t> hud_settings{ 0x5073E70, 0xa8, 0x151F8C };
+vector<intptr_t> subtitle_setting{ 0x5073E80, 0x10A };
+vector<intptr_t> subtitle_show{ 0x5224B90, 0x13e20, 0x332c };
+vector<intptr_t> hud_settings{ 0x5073E80, 0xa8, 0x151F8C };
 
 long long xor_settings = 0x0001000001000101;
+
+bool SelectPressed = false;
+bool L1Pressed = false;
 
 void setHandle() {
     auto hGameWindow = FindWindow(NULL, "MONSTER HUNTER: WORLD(421409)");
@@ -54,16 +47,6 @@ void setHandle() {
     GetWindowThreadProcessId(hGameWindow, &pid);
 
     procHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
-}
-
-HRESULT CreateDeviceHook(
-    REFGUID rguid,
-    LPDIRECTINPUTDEVICE* lplpDirectInputDevice,
-    LPUNKNOWN pUnkOuter
-) {
-    // TODO: start thread here
-    LOG(DEBUG) << "Starting thread";
-    return fnCreateDevice(rguid, lplpDirectInputDevice, pUnkOuter);
 }
 
 void toggleSubtitles() {
@@ -89,8 +72,9 @@ void toggleHUD() {
 
 enum BTN_ACTION { PRESSED, RELEASED, LONG_PRESS, LONG_RELEASE };
 
-BTN_ACTION btnPressed(int btn, bool& pressed, unsigned char input[32]) {
-    if ((int)input[btn] == 0x80) {
+BTN_ACTION btnPressed(Button btn, bool& pressed, uint32_t buttons) {
+    auto button = Buttons[btn];
+    if ((buttons & button) != 0) {
         if (!pressed) {
             pressed = true;
             return BTN_ACTION::PRESSED;
@@ -110,103 +94,28 @@ BTN_ACTION btnPressed(int btn, bool& pressed, unsigned char input[32]) {
     }
 }
 
-BOOL enumerateGameControllers(LPCDIDEVICEINSTANCE devInst, LPVOID devPtr)
-{
-    LPDIRECTINPUTDEVICE8 gamepad = nullptr;
-    if (FAILED(device->CreateDevice(devInst->guidInstance, &gamepad, NULL)))
-    {
-        LOG(ERR) << "Failed to get device " << devInst->tszProductName << " - " << devInst->tszInstanceName;
-    }
-    else
-    {
-        gamepads.push_back(gamepad);
-        //gamepad->SetCooperativeLevel(hGameWindow, DISCL_BACKGROUND | DISCL_EXCLUSIVE);
-        gamepad->SetDataFormat(&c_dfDIJoystick);
+CreateHook((PointerBiConsumer)0x1418e8ea0, PollCtrlHook, void, longlong p1, longlong p2) {
+    Gamepad* gamepad = (Gamepad*)p1;
 
-        LOG(DEBUG) << "Got device " << devInst->tszProductName << " - " << devInst->tszInstanceName;
-    }
-    return DIENUM_CONTINUE;
-}
+    original(p1, p2);
 
-void getGamepads() {
-    if (device->EnumDevices(DI8DEVCLASS_GAMECTRL, &enumerateGameControllers, device, DIEDFL_ATTACHEDONLY) != DI_OK) {
-        LOG(DEBUG) << "Can't find gamepad";
-        device->Release();
-    }
-    //gamepad->Acquire();
-}
-
-void getInput() {
-    memset(gInput, 0, 32);
-    DIJOYSTATE gamepadState;
-    for (auto gamepad : gamepads) {
-        if (FAILED(gamepad->Poll())) {
-            gamepad->Acquire();
-        }
-        else {
-            gamepad->GetDeviceState(sizeof(DIJOYSTATE), &gamepadState);
-            for (int i = 0; i < 32; i++) {
-                gInput[i] |= gamepadState.rgbButtons[i];
-            }
-        }
-    }
-}
-
-void sleep() {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-}
-
-void gamepadLoop() {
-    bool Select = false;
-    bool L1 = false;
-
-    if (procHandle == NULL) {
+    if(procHandle == NULL) {
         setHandle();
     }
 
-    while (gamepads.size() == 0) {
-        getGamepads();
-        sleep();
+    if (btnPressed(Button::Select, SelectPressed, gamepad->buttons) == PRESSED) {
+        LOG(ERR) << std::hex << "Toggle Subtitles: " << Buttons[Button::Select];
+        toggleSubtitles();
     }
-
-    while (gamepads.size() > 0) {
-        getInput();
-        if (btnPressed(6, Select, gInput) == PRESSED) {
-            toggleSubtitles();
-        }
-        auto l1 = btnPressed(4, L1, gInput);
-        if (l1 == PRESSED) {
-            toggleHUD();
-        } else if(l1 == RELEASED) {
-            toggleHUD();
-        }
-        sleep();
+    auto l1 = btnPressed(Button::L1, L1Pressed, gamepad->buttons);
+    if (l1 == PRESSED) {
+        LOG(ERR) << std::hex << "HUD On: " << Buttons[Button::L1];
+        toggleHUD();
+    } else if(l1 == RELEASED) {
+        LOG(ERR) << "HUD Off";
+        toggleHUD();
     }
-    LOG(ERR) << "Terminating... Not checking user input anymore!";
-}
-
-int createDevicesAndHook() {
-    HINSTANCE hInst = (HINSTANCE)GetModuleHandle("MonsterHunterWorld.exe");
-    LOG(DEBUG) << "DInput Got";
-    if (DirectInput8Create(hInst, DIRECTINPUT_VERSION, IID_IDirectInput8, (LPVOID*)&device, NULL) != DI_OK)
-    {
-        LOG(ERR) << "Failed to acquire DirectInput handle";
-        return -1;
-    }
-
-    return 0;
-}
-
-CreateHook(MH::File::LoadResource, StartGamepadThread, void*, void* fileMgr,
-    void* objDef, char* filename, int flag) {
-    void* object = original(fileMgr, objDef, filename, flag);
-    if (flag == 1 && objDef == MH::Quest::QuestNoList::ResourcePtr) {
-        if (!ctrlThread.valid()) {
-            LOG(DEBUG) << "Starting user input thread...";
-            ctrlThread = std::async(std::launch::async, gamepadLoop);
-        }
-    }
-    return object;
+   return;
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -217,9 +126,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        createDevicesAndHook();
         MH_Initialize();
-        QueueHook(StartGamepadThread);
+        QueueHook(PollCtrlHook);
         MH_ApplyQueued();
         break;
     case DLL_THREAD_ATTACH:
