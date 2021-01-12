8832ac3d3bbc70b80ed93249ec3d535922af0f96
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-12 16:33:39 -0500

    refactor!: read/write memory by using base addresses from addresses.json and offsets from member variables
    - Closes #11
    - Closes #14
    
    BREAKING CHANGE: addresses.json no longer contains offsets since these
    don't change. Also, memory.h has a different definition of base and
    requires stuff::memory::exeBase to be defined somewhere.

--- mods/mhw/common/hooky/IHook.cpp
+++ mods/mhw/common/hooky/IHook.cpp
@@ -1,5 +1,10 @@
 #include "IHook.h"
 
+#include "memory/memory.h"
+#pragma comment(lib, "stuff.lib")
+
 namespace MHW {
   IHook::IHook() : addresses(Addy()) {}
 }  // namespace MHW
+
+intptr_t stuff::memory::exeBase = 0x140000000;
--- mods/mhw/common/hooky/IHook.h
+++ mods/mhw/common/hooky/IHook.h
@@ -9,6 +9,5 @@ namespace MHW {
 
    protected:
     IHook();
-    static const intptr_t mhw = 0x140000000;
   };
 }  // namespace MHW
