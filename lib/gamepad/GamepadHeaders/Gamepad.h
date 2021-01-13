1caf1cddc63791977fb974a27d82efed996cabb7
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-12 21:03:10 -0500

    refactor: move source files into src/

--- lib/gamepad/GamepadHeaders/Gamepad.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Button.h"
-#include "Buttons.h"
-#include "GamepadInput.h"
-#include "GamepadInputHelper.h"
-#include "GamepadStruct.h"
-#include "IGamepadDispatcher.h"
--- lib/gamepad/GamepadHeaders/GamepadInput.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-namespace gamepad {
-  typedef uint32_t GamepadInput;
-}
