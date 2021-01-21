2f40795dcf4ec06c0458cbac2b739bbc8ab94486
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-20 22:47:36 -0500

    chore(CMake): update StuffLib and mhw-common and bump up C++ version to C++20

--- mods/mhw/gamepad/src/dllmain.cpp
+++ mods/mhw/gamepad/src/dllmain.cpp
@@ -1,8 +1,6 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 // #include <functional>
 
-#include <MinHook.h>
-
 #include "gamepad.h"
 #pragma comment(lib, "GamepadLib.lib")
 
@@ -13,34 +11,29 @@
 using stuff::json::parseHexString;
 
 using namespace stuff::functions;
+using namespace stuff;
 using namespace gamepad;
 
 PointerBiConsumer original = nullptr;
-
 void PollCtrlHook(long long p1, long long p2) {
   original(p1, p2);
   gamepad::GetDispatcher().update(*(Gamepad*)p1);
   return;
 }
 
-void hookem() {
-  auto PollCtrl = parseHexString(MHW::loadConfig("addresses.json")["PollController()"]);
-  MH_Initialize();
-  // PointerBiConsumer PollCtrl = nullptr;
-  // auto hook = std::bind_front(PollCtrlHook, PollCtrl);
-  QueueHook(PollCtrl, &PollCtrlHook, &original);
-  MH_ApplyQueued();
-}
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
   switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH: {
-      hookem();
+      auto PollCtrl = parseHexString(MHW::loadConfig("addresses.json")["PollController()"]);
+      hook::init();
+      hook::queue(PollCtrl, &PollCtrlHook, &original);
+      hook::apply();
       break;
     }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
+      hook::unhook();
       break;
   }
   return TRUE;
