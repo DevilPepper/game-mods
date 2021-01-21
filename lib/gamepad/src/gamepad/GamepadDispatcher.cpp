78026c520f56adcc116484a8b6308a9ce22f6e3a
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-21 03:06:25 -0500

    chore: clean up console output that's no longer needed

--- lib/gamepad/src/gamepad/GamepadDispatcher.cpp
+++ lib/gamepad/src/gamepad/GamepadDispatcher.cpp
@@ -1,22 +1,15 @@
 #include "GamepadDispatcher.h"
 
-#include <algorithm>
-#include <functional>
-#include <iostream>
-#include <vector>
-
 namespace gamepad {
   GamepadDispatcher::GamepadDispatcher() : callbacks(std::list<GamepadCallback>()) {}
 
   GamepadToken GamepadDispatcher::registerCallback(GamepadCallback callback) {
     callbacks.push_front(callback);
-    std::cout << "Registered callback. # registered: " << callbacks.size() << std::endl;
     return callbacks.begin();
   }
 
   IGamepadDispatcher& GamepadDispatcher::unregisterCallback(GamepadToken token) {
     callbacks.erase(token);
-    std::cout << "Unregistered callback. # registered: " << callbacks.size() << std::endl;
     return *this;
   }
 
