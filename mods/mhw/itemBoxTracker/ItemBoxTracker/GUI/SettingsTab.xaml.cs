b8f84edd60dafb8f52bdca28bb887f6e01081eba
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-05-06 04:04:54 -0400

    feat(SetingsTab): add up and down buttons and some tooltips

--- mods/mhw/itemBoxTracker/ItemBoxTracker/GUI/SettingsTab.xaml.cs
+++ mods/mhw/itemBoxTracker/ItemBoxTracker/GUI/SettingsTab.xaml.cs
@@ -16,14 +16,36 @@ namespace MHWItemBoxTracker.GUI {
 
     private void DeleteRow(object sender, RoutedEventArgs e) {
       var item = ((FrameworkElement)sender).DataContext as ItemViewModel;
-      ((TrackingTabViewModel)DataContext).Tracking.Remove(item);
+      var Tracking = ((TrackingTabViewModel)DataContext).Tracking;
+      Tracking.Remove(item);
+      if (Tracking.Count == 0) {
+        Tracking.Add(new ItemViewModel());
+      }
     }
     private void AddRow(object sender, RoutedEventArgs e) {
       ((TrackingTabViewModel)DataContext).Tracking.Add(new ItemViewModel());
     }
+    private void MoveUp(object sender, RoutedEventArgs e) {
+      var item = ((FrameworkElement)sender).DataContext as ItemViewModel;
+      var Tracking = ((TrackingTabViewModel)DataContext).Tracking;
+      var index = Tracking.IndexOf(item);
+      if (index > 0) {
+        Tracking.Move(index, index - 1);
+      }
+    }
+
+    private void MoveDown(object sender, RoutedEventArgs e) {
+      var item = ((FrameworkElement)sender).DataContext as ItemViewModel;
+      var Tracking = ((TrackingTabViewModel)DataContext).Tracking;
+      var index = Tracking.IndexOf(item);
+      if (index < Tracking.Count - 1) {
+        Tracking.Move(index, index + 1);
+      }
+    }
 
     private void OnTextChanged(ObservableCollection<object> suggestions, string input) {
-      // TODO: Filter items from HunterPie
+      // TODO: Filter items from HunterPie based on search
+      // filter out items already in ((TrackingTabViewModel)DataContext).Tracking
       suggestions.Clear();
     }
     private void OnSuggestionChosen(object selection, object choice) {
