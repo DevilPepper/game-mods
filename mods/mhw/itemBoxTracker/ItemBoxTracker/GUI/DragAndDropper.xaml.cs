390c1377505ab32f3e2ffcde3b7455ccff0a2c9c
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-05-06 01:07:06 -0400

    feat(DragAndDropper): add DragAndDropper higher order component for ListViews

---
+++ mods/mhw/itemBoxTracker/ItemBoxTracker/GUI/DragAndDropper.xaml.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MHWItemBoxTracker.GUI {
+  // TODO: revisit this some day
+  public partial class DragAndDropper : UserControl {
+
+    public ListView Child {
+      get { return (ListView)GetValue(ChildProperty); }
+      set { SetValue(ChildProperty, value); }
+    }
+    public static readonly DependencyProperty ChildProperty = DependencyProperty.Register(
+    "Child", typeof(ListView), typeof(DragAndDropper), new PropertyMetadata(null));
+    public DragAndDropper() : base() {
+      InitializeComponent();
+    }
+  }
+}
