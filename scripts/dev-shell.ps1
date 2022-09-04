25775f820ebc96259627c3946fb3d83e1a0a50a4
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2022-09-04 19:14:21 -0400

    chore(CI): remove fancy stuff

--- scripts/dev-shell.ps1
+++
@@ -1,3 +0,0 @@
-$MSVS = vswhere.exe -prerelease -latest -property installationPath
-Import-Module $MSVS\Common7\Tools\Microsoft.VisualStudio.DevShell.dll
-Enter-VsDevShell -SkipAutomaticLocation -DevCmdArguments '-arch=x64 -no_logo' -InstallPath $MSVS
